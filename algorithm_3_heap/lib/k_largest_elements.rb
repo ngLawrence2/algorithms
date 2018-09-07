d16d63ecfaff352c848431712dd44e9469480950
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-06 21:00:55 -0700

    quicksort

--- algorithm_3_heap/lib/k_largest_elements.rb
+++ algorithm_3_heap/lib/k_largest_elements.rb
@@ -1,22 +1,28 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-  res = []
-  prc = Proc.new do |el1, el2|
-    -1 * (el1 <=> el2)
-  end
-
+  # res = []
+  # prc = Proc.new do |el1, el2|
+  #   -1 * (el1 <=> el2)
+  # end
+  #
+  # heap = BinaryMinHeap.new
+  # array.each_with_index do |k,idx|
+  #   heap.store << k
+  #   BinaryMinHeap.heapify_up(heap.store,idx, &prc)
+  #
+  # end
+  #
+  # until res.length == k
+  #   val = heap.extract
+  #   res << val
+  #   BinaryMinHeap.heapify_down(heap.store,0, &prc)
+  # end
+  # res
   heap = BinaryMinHeap.new
-  array.each_with_index do |k,idx|
-    heap.store << k
-    BinaryMinHeap.heapify_up(heap.store,idx, &prc)
-  
+  array.each do |el|
+    heap.push(el)
+    heap.extract if(heap.count>k)
   end
-
-  until res.length == k
-    val = heap.extract
-    res << val
-    BinaryMinHeap.heapify_down(heap.store,0, &prc)
-  end
-  res
+  heap.store
 end
---
+++ algorithm_4/lib/quick_sort.rb
@@ -0,0 +1,45 @@
+class QuickSort
+  # Quick sort has average case time complexity O(nlogn), but worst
+  # case O(n**2).
+
+  # Not in-place. Uses O(n) memory.
+  def self.sort1(array)
+    return array if self.length <=1
+    pivotEl=array[0]
+    left = array[1..-1].select{|el| el <=pivotEl}
+    right=array[1..-1].select{|el| el >pivotEl}
+    QuickSort.sort1(left)+[pivotEl]+QuickSort.sort1(right)
+  end
+
+  # In-place.
+  def self.sort2!(array, start = 0, length = array.length, &prc)
+    return array if (length-start)<=1
+    prc ||= Proc.new{ |a,b| a<=>b}
+    if start < length
+      randPiv = rand(start..length)
+      pivotIdx = QuickSort.partition(array,start,length,&prc)
+      QuickSort.sort2!(array, start, pivotIdx, &prc)
+      QuickSort.sort2!(array,pivotIdx+1,length,&prc)
+    end
+    array
+  end
+
+  def self.partition(array, start, length, &prc)
+    prc||=Proc.new{|a,b|a<=>b}
+    pivotIdx=start
+    start+=1
+    partitionIdx = start
+     until start >= (pivotIdx+length)
+      val = array[start]
+      if val && prc.call(val,array[pivotIdx]) < 0
+        array[partitionIdx],array[start]=array[start],array[partitionIdx]
+        partitionIdx+=1
+        start+=1
+      else #val is greater than pivotIdx
+        start+=1
+      end
+    end
+      array[pivotIdx],array[partitionIdx-1]=array[partitionIdx-1],array[pivotIdx]
+      partitionIdx-=1
+  end
+end
