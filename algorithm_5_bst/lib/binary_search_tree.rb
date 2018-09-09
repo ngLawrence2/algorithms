9a59435283817a6679508c3cb339601a8736867b
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-09 14:32:49 -0700

    depth

--- algorithm_5_bst/lib/binary_search_tree.rb
+++ algorithm_5_bst/lib/binary_search_tree.rb
@@ -61,9 +61,18 @@ class BinarySearchTree
       if tree_node.left.value == value
         deleteNode = tree_node.left
         # node to delete has no child
-        if deleteNode.left.nil? && tree_node.right.nil?
+        if deleteNode.left.nil? && deleteNode.right.nil?
           tree_node.left=nil
           return
+        end
+        if deleteNode.left.nil?
+          #we have a right child
+          tree_node.right = deleteNode.right
+        elsif deleteNode.right.nil?
+          #we have a left child
+          tree_node.left=deleteNode.left
+        else
+          #we have both child
         end
       else
         delete_node(value,tree_node.left)
@@ -72,26 +81,48 @@ class BinarySearchTree
       if tree_node.right.value == value
           #check if we have any children
           deleteNode=tree_node.right
-          if deleteNode.left.nil? && delete_node.right.nil?
+          if deleteNode.left.nil? && deleteNode.right.nil?
             tree_node.right=nil
             return
+          end
+          if deleteNode.left.nil?
+            #we have a right child
+            tree_node.right = deleteNode.right
+            return
+          elsif deleteNode.right.nil?
+            #we have a left child
+            tree_node.left= deleteNode.left
+          else
+            #we have both child
           end
       else
         delete_node(value,tree_node.right)
       end
     end
   end
+
   # helper method for #delete:
   def maximum(tree_node = @root)
+    return tree_node if tree_node.right.nil?
+    maximum(tree_node.right)
   end
 
   def depth(tree_node = @root)
+    return 0 if tree_node.nil? || (tree_node.right.nil? && tree_node.left.nil?)
+    right=depth(tree_node.right)+1
+    left=depth(tree_node.left)+1
+    [left,right].max
   end
 
   def is_balanced?(tree_node = @root)
+    left = depth(tree_node.left)
+    right = depth(tree_node.right)
+    return false if (left-right).abs > 1
+    return is_balanced?(tree_node.left) && is_balanced?(tree_node.right)
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+
   end
 
 
