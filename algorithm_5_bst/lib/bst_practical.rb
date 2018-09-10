0603a2a2f2b87b30f85e6c5a2a3c8352decd8504
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-10 08:35:25 -0700

    reverse traversal for kth largest

--- algorithm_5_bst/lib/bst_practical.rb
+++ algorithm_5_bst/lib/bst_practical.rb
@@ -1,9 +1,14 @@
-require 'binary_search_tree'
-
-def kth_largest(tree_node, k)
-  tree=BinarySearchTree.new
-  tree.root=tree_node
-  arr = tree.in_order_traversal
-  value = arr[arr.length-k]
-  tree.find(value)
+def kth_largest(tree_node, k, arr=[])
+  unless tree_node.right.nil?
+    kth_largest(tree_node.right,k,arr)
+      return arr.last if arr.count==k
+  end
+  arr << tree_node
+  return arr.last if arr.count==k
+  unless tree_node.left.nil?
+    kth_largest(tree_node.left,k,arr)
+      return arr.last if arr.count==k
+      p "this is k " + k.to_s
+      p "arr length " + arr.count.to_s
+  end
 end
