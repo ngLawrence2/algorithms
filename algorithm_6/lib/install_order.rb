4fa3284af8764aa96372bd17fadecdd7937656c5
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-11 09:37:27 -0700

    graph

--- algorithm_6/lib/install_order.rb
+++ algorithm_6/lib/install_order.rb
@@ -7,8 +7,22 @@
 
 # Import any files you need to
 
-
+require_relative 'topological_sort'
 
 def install_order(arr)
-
+  hash = {}
+  packages = {}
+  edge = []
+  arr.each do |k|
+    package1 = k[0]
+    package2 = k[1]
+    hash[package1] = 1
+    hash[package2] = 1
+    packages[package1] = Vertex.new(package1) if hash[package1]
+    packages[package2] = Vertex.new(package2) if hash[package2]
+    edge << Edge.new(packages[package1], packages[package2])
+  end
+  packages["key"] = Vertex.new(8)
+  packages["key2"] = Vertex.new(7)
+  result = kahn(packages.values)
 end
--- algorithm_6/lib/topological_sort.rb
+++ algorithm_6/lib/topological_sort.rb
@@ -2,11 +2,12 @@ require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
-def khan(vertices)
+def kahn(vertices)
   sorted = []
   queue = []
   vertices.each do |vertex|
     queue << vertex if vertex.in_edges.empty?
+    p vertex.value
   end
   until queue.empty?
     current = queue.shift
@@ -31,18 +32,22 @@ end
 def topological_sort(vertices)
   sorted = []
   visited = Hash.new { |h,v| h[v]=false}
-  notVisited = Hash.new { |h,v| h[v]=false}
+  recursionStack=Hash.new{|h,v| h[v]=false}
   vertices.each do |vertex|
-    visit(vertex,sorted,visited) if vertex.in_edges.empty?
+    visit(vertex,sorted,visited, recursionStack) if vertex.in_edges.empty?
   end
   sorted
 end
 
-def visit(vertex, result=[], visited)
+def visit(vertex, result, visited, recursionStack)
+  return [] if recursionStack[vertex.value]
   return if visited[vertex.value]==true
-  vertex.out_edges.each do |edge|
-    visit(edge.to_vertex,result,visited)
-  end
   visited[vertex.value]=true
+  recursionStack[vertex.value]=true
+  vertex.out_edges.each do |edge|
+    visit(edge.to_vertex,result,visited,recursionStack)
+  end
+  recursionStack[vertex.value]=false
+
   result.unshift(vertex)
 end
