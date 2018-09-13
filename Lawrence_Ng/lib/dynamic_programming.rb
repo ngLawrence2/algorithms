b7e43931991fea873df583a142823e5dd11facfc
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-13 08:13:37 -0700

    knapsack

--- Lawrence_Ng/lib/dynamic_programming.rb
+++ Lawrence_Ng/lib/dynamic_programming.rb
@@ -1,8 +1,12 @@
+require 'byebug'
+
 class DynamicProgramming
 
   def initialize
     @blair_nums={ 1=>1, 2=>2}
     @frog_top_down = { 1 => [[1]], 2 => [[1,1],[2]], 3 => [[1,1,1], [1,2], [2,1], [3]]  }
+    @super_frog={}
+    @knapsack=[]
   end
 
   #get nth odd number
@@ -80,24 +84,60 @@ class DynamicProgramming
   end
 
   def super_frog_hops(n, k)
-    return super_frog_cache_builder(n,k)
-  end
-
-  def super_frog_cache_builder(n,k)
-    cache = { 1 => [[1]]}
-    (2..n).each do |array|
-
+    cache = Hash.new { |h,k| h[k]=[]}
+    (1..n).each do |i|
+      cache[i] << [i] if i <=k
+      var = i > k ? k : i - 1
+      (1..var).each do |val|
+        cache[i] += cache[i-val].map{|el| el + [val]}
+      end
     end
+    cache[n]
   end
 
   def knapsack(weights, values, capacity)
-
+    table = knapsack_table(weights,values,capacity)
+    table[-1][-1]
   end
 
   # Helper method for bottom-up implementation
   def knapsack_table(weights, values, capacity)
+    table = Array.new(weights.length) { [0]}
+    (0..values.length-1).each do |currentItem|
+      (1..capacity).each do |currentWeight|
 
+          if (currentWeight < weights[currentItem])
+            if currentItem == 0
+              table[currentItem] << 0
+            else
+              table[currentItem] << table[currentItem-1][currentWeight]
+            end
+          else
+            #current weight is bigger than weight
+            #check for if we are at the top row
+            if currentItem == 0
+              table[currentItem] << values[currentItem]
+            else
+              difference = currentWeight - weights[currentItem]
+              leftover = table[currentItem-1][difference] + values[currentItem]
+              prevResult = table[currentItem-1][currentWeight]
+              table[currentItem] << [leftover,prevResult].max
+            end
+          end
+      end
+    end
+    table
   end
+
+
+def getMaxValueFromWeight(weights,values,capacity)
+  result = 0
+  (0..values.length-1).each do |idx|
+    result = values[idx] if (values[idx]>result && weights[idx]<=capacity )
+  end
+  result
+end
+
 
   def maze_solver(maze, start_pos, end_pos)
   end
